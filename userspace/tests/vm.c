9fd62361f10801bb4757528e1358a91c8082632b
Fabian Rauscher <fabian.rauscher@tugraz.at>
2026-02-27 12:42:36 +0100

    .

--- userspace/tests/vm.c
+++ userspace/tests/vm.c
@@ -1,8 +1,25 @@
 #include "../../../common/include/kernel/syscall-definitions.h"
 #include "sys/syscall.h"
+#include "vm.h"
+
+extern "C" void test_end();
+
+__attribute__((naked))
+void test_begin() {
+  asm volatile(R"(
+  .code32
+  //your asm here
+
+  .code64
+  .global test_end
+  test_end:
+  )");
+}
+
+#define TEST_SIZE ((pointer)test_end - (pointer)test_begin)
 
 int main(int argc, char *argv[]) {
-    // Implement an interface to **nicely** start virtualized SWEB instances
-    __syscall(sc_hypervisor_init_guest, 0, 0, 0, 0, 0);
+    assert(vm_launch_mem((void*)test_begin, TEST_SIZE) == 0);
+    //assert(vm_launch_mb("/boot/boot/kernel.x"); // this is how you launch sweb
     return 0;
 }
